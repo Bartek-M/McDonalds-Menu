dccfd681b351afaca3bd67ded7d634f9c510b568
Bartek <bmroczkowski@icloud.com>
2025-01-07 08:09:21 +0100

    Add scolling animations. Bug fixes.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,66 @@
-window.onload = () => {
-    const categoriesWrapper = document.querySelector(".categories-wrapper");
-    const titleHeader = document.querySelector(".title-header");
-    const navBack = document.querySelector(".nav-back")
+const categoriesWrapper = document.querySelector(".categories-wrapper");
+const titleHeader = document.querySelector(".title-header");
+const navBack = document.querySelector(".nav-back")
 
-    // Select all category titles and category buttons
-    const titles = document.querySelectorAll(".category-title");
-    const buttons = document.querySelectorAll(".categories button");
+const titles = document.querySelectorAll(".items-wrapper");
+const buttons = document.querySelectorAll(".categories button");
 
-    const margin = 300;
+function scrollToActiveButton(activeButton) {
+    let wrapperRect = categoriesWrapper.getBoundingClientRect();
+    let buttonRect = activeButton.getBoundingClientRect();
 
-    window.addEventListener("scroll", () => {
-        let offset = titleHeader.getBoundingClientRect().bottom;
-
-        if (offset <= 160) {
-            categoriesWrapper.classList.add("sticky");
-            navBack.classList.add("sticky")
-        } else {
-            categoriesWrapper.classList.remove("sticky");
-            navBack.classList.remove("sticky")
-        }
-
-        let observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                let title = entry.target;
-                let button = document.querySelector(`.categories button[id="for-${title.id}"]`);
-
-                let titleRect = title.getBoundingClientRect();
-                let middleOfViewport = window.innerHeight / 2;
-
-                if (titleRect.top < middleOfViewport + 50 && titleRect.bottom > middleOfViewport - 50) {
-                    buttons.forEach(btn => btn.classList.remove("active"));
-                    button.classList.add("active");
-                }
-            });
-        }, {
-            threshold: 0.5
+    if (buttonRect.left < wrapperRect.left || buttonRect.right > wrapperRect.right) {
+        categoriesWrapper.scrollBy({
+            left: buttonRect.left - wrapperRect.left - (wrapperRect.width / 2) + (buttonRect.width / 2),
+            behavior: "smooth"
         });
+    }
+}
 
-        titles.forEach(title => observer.observe(title));
-    })
+window.addEventListener("scroll", () => {
+    let offset = titleHeader.getBoundingClientRect().bottom;
 
-    buttons.forEach(btn => {
-        btn.addEventListener("click", () => {
-            const targetId = btn.id.slice(4);
-            const targetCategory = document.getElementById(targetId);
+    if (offset <= 160) {
+        categoriesWrapper.classList.add("sticky");
+        navBack.classList.add("sticky")
+    } else {
+        categoriesWrapper.classList.remove("sticky");
+        navBack.classList.remove("sticky")
+    }
 
-            if (targetCategory) {
-                window.scrollTo({
-                    top: targetCategory.offsetTop - margin,
-                    behavior: "smooth",
-                });
+    let observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            let title = entry.target;
+            let button = document.querySelector(`.categories button[id="for-${title.id}"]`);
+
+            let titleRect = title.getBoundingClientRect();
+            let middleOfViewport = window.innerHeight / 2;
+
+            if (titleRect.top < middleOfViewport + 50 && titleRect.bottom > middleOfViewport - 50) {
+                buttons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                scrollToActiveButton(button);
             }
         });
+    }, {
+        threshold: 0.5
     });
-};
+
+    titles.forEach(title => observer.observe(title));
+})
+
+const margin = 300;
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const targetId = btn.id.slice(4);
+        const targetCategory = document.getElementById(targetId);
+
+        if (targetCategory) {
+            window.scrollTo({
+                top: targetCategory.offsetTop - margin,
+                behavior: "smooth",
+            });
+        }
+    });
+});
